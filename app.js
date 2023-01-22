1243f6442dbf6ed108d3401c8667a990f6edf894
Sam <transferdata76@gmail.com>
2023-01-21 17:45:41 -0800

    created creature class

---
+++ app.js
@@ -0,0 +1,11 @@
+class creatureOne {
+    constructor(name, hunger, sleepiness, boredom, age) {
+        this.name = name;
+        this.hunger = 1;
+        this.sleepiness = 1;
+        this.boredom = 0;
+        this.age = 0;
+    }
+}
+
+
