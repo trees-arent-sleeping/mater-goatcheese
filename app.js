9663a4fc5e9c6c34e1b7bfa6f9e74faee0b4cf5b
Sam <transferdata76@gmail.com>
2023-01-22 12:34:26 -0800

    animated the passage of time

--- app.js
+++ app.js
@@ -12,9 +12,17 @@ class creatureOne {
 let creatureName = "ro"//prompt("What is your creature's name?")
 let creatureProtagonist = new creatureOne(creatureName)
 let creatureStats = document.getElementById("stats")
+let hand = document.getElementById("fingerTime")
 creatureStats.children[0].innerHTML = `Hunger: ${creatureProtagonist.hunger}`
 creatureStats.children[1].innerHTML = `Sleepiness: ${creatureProtagonist.sleepiness}`
 creatureStats.children[2].innerHTML = `Boredom: ${creatureProtagonist.boredom}`
 creatureStats.children[3].innerHTML = `Age: ${creatureProtagonist.age}`
+
+degrees = 0
+function passTime() {
+    degrees += 0.1875
+    hand.style.transform = `rotate(${degrees}deg)`
+}
+setInterval(passTime, 31.25)
 
 console.log(creatureStats.children[0])
