f440f02c96716e3abd744a46e90e1f7c8464ad35
Sam <transferdata76@gmail.com>
2023-01-23 18:17:41 -0800

    animated user input, added game over

--- app.js
+++ app.js
@@ -8,39 +8,87 @@ class creatureOne {
     }
 }
 
-
 let creatureName = "ro"//prompt("What is your creature's name?")
 let creatureProtagonist = new creatureOne(creatureName)
 let creatureStats = document.getElementById("stats")
 let hand = document.getElementById("fingerTime")
-
+let head = document.getElementById("fishHead")
 
 let daysDisplay = document.getElementById("days")
 let daysNum = 0
-degrees = 0
+
+let controls = document.getElementById("controls")
+function splashButton(x) {
+    creatureStats.children[x].style.filter = `hue-rotate(${Math.random()*360}rad)`;
+    controls.children[x].style.filter = `hue-rotate(${Math.random()*360}deg)`;
+}
+
+controls.children[0].addEventListener("click", ()=>{
+    if (creatureProtagonist.hunger != 0) {
+        creatureProtagonist.hunger --;
+        splashButton(0)
+    }
+
+})
+controls.children[1].addEventListener("click",()=>{
+    if (creatureProtagonist.rot != 0) {
+        creatureProtagonist.rot --;
+        splashButton(1);
+    }
+})
+controls.children[2].addEventListener("click",()=>{
+    if (creatureProtagonist.boredom != 0) {
+        creatureProtagonist.boredom --;
+        splashButton(2);
+    }
+});
+
 
 function updateStats() {
     creatureStats.children[0].innerHTML = `Hunger: ${creatureProtagonist.hunger}`
+    creatureStats.children[0].style.transform = `scale(${1+creatureProtagonist.hunger/20})`
+
     creatureStats.children[1].innerHTML = `Rot: ${creatureProtagonist.rot}`
+    creatureStats.children[1].style.transform = `scale(${1+creatureProtagonist.rot/20})`
+
     creatureStats.children[2].innerHTML = `Boredom: ${creatureProtagonist.boredom}`
+    creatureStats.children[2].style.transform = `scale(${1+creatureProtagonist.boredom/20})`
     creatureStats.children[3].innerHTML = `Age: ${creatureProtagonist.age}`
 }
 
+degrees = 18
 function passTime() {
-    degrees += 6
+    degrees += 24
     hand.style.transform = `rotate(${degrees}deg)`
-    daysNum += 3.75
-    daysDisplay.innerHTML = `Days: ${Math.floor(daysNum)}`;
-    if (degrees % 378 == 0) {
-        creatureProtagonist.age +=1
-        creatureProtagonist.boredom +=2
-        creatureProtagonist.rot +=3
+    if (daysNum % 75 == 0) {
+        creatureProtagonist.rot += 1
     }
-    if (daysNum % 112.5 == 0) {
-        creatureProtagonist.hunger += 1
-    }
-    hand.style.filter = `hue-rotate(${degrees}deg)`
-    updateStats()
+    hand.style.filter = `hue-rotate(${daysNum}deg)`
 }
-setInterval(passTime, 250)
+setInterval(()=>{
+    daysNum += 1
+    if (daysNum % 365 == 0) {
+        creatureProtagonist.age +=1
+        splashButton(3)
+        degrees = 18
+    }
+    if (daysNum % 73 == 0) {
+        creatureProtagonist.hunger += 1
+        creatureProtagonist.boredom +=1
+        creatureProtagonist.rot +=2
+    }
+    daysDisplay.innerHTML = `Days: ${Math.floor(daysNum)}`;
+    updateStats();
+    if (creatureProtagonist.boredom == 10) {
+        alert("failed to boredom")
+    if (creatureProtagonist.rot == 10) {
+        alert("failed to rot")
+    if (creatureProtagonist.boredom == 10) {
+        alert("failed to boredom")
+    }
+    } 
+    }
+},41.0958904109589)
+setInterval(passTime, 1000)
+
 
